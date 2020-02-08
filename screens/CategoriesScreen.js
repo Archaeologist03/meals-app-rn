48678a302eed4dcd30c6447d65abe5ea002d9598
Archaeologist03 <omniiknight@hotmail.com>
2020-02-08 23:54:07 +0100

    Lection 19 - Params with navigation, dynamic and default nav options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,24 +5,26 @@ import {
 	FlatList,
 	StyleSheet,
 	TouchableOpacity,
-	Platform,
 } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
 	const renderGridItem = itemData => {
 		return (
-			<TouchableOpacity
-				style={styles.gridItem}
-				onPress={() => {
-					props.navigation.navigate({ routeName: 'CategoryMeals' });
-				}}>
-				<View>
-					<Text>{itemData.item.title}</Text>
-				</View>
-			</TouchableOpacity>
+			<CategoryGridTile
+				title={itemData.item.title}
+				color={itemData.item.color}
+				onSelect={() => {
+					props.navigation.navigate({
+						routeName: 'CategoryMeals',
+						params: {
+							categoryId: itemData.item.id,
+						},
+					});
+				}}
+			/>
 		);
 	};
 
@@ -38,10 +40,6 @@ const CategoriesScreen = props => {
 
 CategoriesScreen.navigationOptions = {
 	headerTitle: 'Meals Category',
-	headerStyle: {
-		backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
-	},
-	headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
