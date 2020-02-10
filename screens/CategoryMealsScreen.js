a0fa5168afda38374147d23857de3663e02c124d
Archaeologist03 <omniiknight@hotmail.com>
2020-02-10 18:11:15 +0100

    Lection 26, Header Buttons, Tabs based navigation

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,35 +2,50 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
-const CategoryMealsScreen = props => {
+const CategoryMealScreen = props => {
 	const renderMealItem = itemData => {
 		return (
-			<View>
-				<Text>{itemData.item.title}</Text>
-			</View>
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {
+					props.navigation.navigate({
+						routeName: 'MealDetail',
+						params: {
+							mealId: itemData.item.id,
+						},
+					});
+				}}
+			/>
 		);
 	};
 
 	const catId = props.navigation.getParam('categoryId');
 
-	const dispplayedMeals = MEALS.filter(
+	const displayedMeals = MEALS.filter(
 		meal => meal.categoryIds.indexOf(catId) >= 0,
 	);
 
 	return (
 		<View style={styles.screen}>
 			<FlatList
-				data={dispplayedMeals}
+				data={displayedMeals}
 				keyExtractor={(item, index) => item.id}
 				renderItem={renderMealItem}
+				style={{ width: '100%' }}
 			/>
 		</View>
 	);
 };
 
-CategoryMealsScreen.navigationOptions = navigationData => {
+CategoryMealScreen.navigationOptions = navigationData => {
 	const catId = navigationData.navigation.getParam('categoryId');
+
 	const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
 	return {
@@ -43,7 +58,8 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
+		padding: 15,
 	},
 });
 
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
